35651cdd4e926dd890d30c5e5d6be56cbdde8c26
chengenzhao <chengen.zhao@mail.mcgill.ca>
2025-09-01 19:30:21 +0800

    refactor

--- src/main/java/com/whitewoodcity/fxgl/vectorview/VectorGraphics.java
+++ src/main/java/com/whitewoodcity/fxgl/vectorview/VectorGraphics.java
@@ -4,29 +4,35 @@ import module java.base;
 import module javafx.controls;
 
 public class VectorGraphics extends Group {
-  private List<SVGLayer> layers = new ArrayList<>();
-
-  public VectorGraphics() {
-    this.getChildren().addAll(layers);
-  }
 
   public void trim(){
     var p = getXY();
-    for(var layer:layers)
-      layer.trim(p.getX(),p.getY());
+    getChildren().stream()
+      .filter(SVGLayer.class::isInstance)
+      .map(SVGLayer.class::cast)
+      .forEach(e-> e.trim(p));
+  }
+
+  public void move(Point2D p){
+    move(p.getX(), p.getY());
   }
 
   public void move(double x, double y){
-    layers.forEach(e -> e.move(x, y));
+    getChildren().stream()
+      .filter(SVGLayer.class::isInstance)
+      .map(SVGLayer.class::cast)
+      .forEach(e -> e.move(x, y));
   }
 
   public Point2D getXY(){
     double x = 0;
     double y = 0;
-    for(var layer:layers){
-      var p = layer.getMinXY();
-      x = Math.min(x, p.getX());
-      y = Math.min(y, p.getY());
+    for(var node:getChildren()){
+      if(node instanceof SVGLayer layer){
+        var p = layer.getMinXY();
+        x = Math.min(x, p.getX());
+        y = Math.min(y, p.getY());
+      }
     }
     return new Point2D(x,y);
   }
@@ -34,10 +40,12 @@ public class VectorGraphics extends Group {
   public Dimension2D getDimension(){
     double w = 0;
     double h = 0;
-    for(var layer:layers){
-      var d = layer.getDimension();
-      w = Math.max(w, d.getWidth());
-      h = Math.max(h, d.getHeight());
+    for(var node:getChildren()){
+      if(node instanceof SVGLayer layer) {
+        var d = layer.getDimension();
+        w = Math.max(w, d.getWidth());
+        h = Math.max(h, d.getHeight());
+      }
     }
     return new Dimension2D(w,h);
   }
