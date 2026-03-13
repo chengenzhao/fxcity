e77b868ce0f8f89450beb2ca9011fdbf44315388
chengenzhao <chengen.zhao@mail.mcgill.ca>
2026-03-13 20:29:47 +0800

    releasing 1.1.2

--- src/main/java/com/whitewoodcity/fxgl/vectorview/JVGLayer.java
+++ src/main/java/com/whitewoodcity/fxgl/vectorview/JVGLayer.java
@@ -1,9 +1,16 @@
 package com.whitewoodcity.fxgl.vectorview;
 
 import module com.fasterxml.jackson.databind;
-import module javafx.controls;
 import module java.base;
+import module javafx.controls;
 import com.whitewoodcity.fxgl.vectorview.svgpathcommand.*;
+import javafx.geometry.Dimension2D;
+import javafx.geometry.Point2D;
+import javafx.scene.effect.GaussianBlur;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.RadialGradient;
+import javafx.scene.paint.Stop;
 
 public class JVGLayer extends SVGPath {
 
@@ -42,8 +49,7 @@ public class JVGLayer extends SVGPath {
     NO_CYCLE(_NO_CYCLE),
     REFLECT(_REFLECT),
     REPEAT(_REPEAT),
-    GRADIENT_TYPE("gradientType")
-    ;
+    GRADIENT_TYPE("gradientType");
 
     private final String key;
 
@@ -168,6 +174,26 @@ public class JVGLayer extends SVGPath {
 
   public JVGLayer move(double x, double y) {
     map(p -> p.get() + x, p -> p.get() + y);
+
+    switch (this.getFill()) {
+      case LinearGradient gradient -> {
+        var sx = gradient.getStartX();
+        var sy = gradient.getStartY();
+        var ex = gradient.getEndX();
+        var ey = gradient.getEndY();
+
+        this.setFill(new LinearGradient(sx + x, sy + y, ex + x, ey + y, gradient.isProportional(), gradient.getCycleMethod(), gradient.getStops()));
+      }
+      case RadialGradient gradient -> {
+        var cx = gradient.getCenterX();
+        var cy = gradient.getCenterY();
+
+        this.setFill(new RadialGradient(gradient.getFocusAngle(), gradient.getFocusDistance(), cx + x, cy + y, gradient.getRadius(), gradient.isProportional(), gradient.getCycleMethod(), gradient.getStops()));
+      }
+      default -> {
+      }
+    }
+
     return this;
   }
 
@@ -179,6 +205,26 @@ public class JVGLayer extends SVGPath {
       case null, default -> {
       }
     }
+
+    switch (this.getFill()) {
+      case LinearGradient gradient -> {
+        var sx = gradient.getStartX();
+        var sy = gradient.getStartY();
+        var ex = gradient.getEndX();
+        var ey = gradient.getEndY();
+
+        this.setFill(new LinearGradient(sx * factor, sy * factor, ex * factor, ey * factor, gradient.isProportional(), gradient.getCycleMethod(), gradient.getStops()));
+      }
+      case RadialGradient gradient -> {
+        var cx = gradient.getCenterX();
+        var cy = gradient.getCenterY();
+
+        this.setFill(new RadialGradient(gradient.getFocusAngle(), gradient.getFocusDistance() * factor, cx * factor, cy * factor, gradient.getRadius() * factor, gradient.isProportional(), gradient.getCycleMethod(), gradient.getStops()));
+      }
+      default -> {
+      }
+    }
+
     return this;
   }
 
@@ -194,7 +240,7 @@ public class JVGLayer extends SVGPath {
     var objectNode = mapper.createObjectNode();
 
     objectNode.put(JsonKeys.STROKE_WIDTH.key, getStrokeWidth());
-    switch (getFill()){
+    switch (getFill()) {
       case Color color -> objectNode.put(JsonKeys.FILL.key, toWebHexWithAlpha(color));
       case LinearGradient gradient -> {
         var g = mapper.createObjectNode();
@@ -204,13 +250,13 @@ public class JVGLayer extends SVGPath {
         g.put(JsonKeys.END_X.key, gradient.getEndX());
         g.put(JsonKeys.END_Y.key, gradient.getEndY());
         g.put(JsonKeys.PROPORTIONAL.key, gradient.isProportional());
-        g.put(JsonKeys.CYCLE_METHOD.key, switch (gradient.getCycleMethod()){
-          case NO_CYCLE ->  _NO_CYCLE;
-          case REFLECT ->  _REFLECT;
+        g.put(JsonKeys.CYCLE_METHOD.key, switch (gradient.getCycleMethod()) {
+          case NO_CYCLE -> _NO_CYCLE;
+          case REFLECT -> _REFLECT;
           case REPEAT -> _REPEAT;
         });
         var stops = mapper.createArrayNode();
-        for(var stop:gradient.getStops()){
+        for (var stop : gradient.getStops()) {
           var s = mapper.createObjectNode();
           s.put(JsonKeys.OFFSET.key, stop.getOffset());
           s.put(JsonKeys.COLOR.key, toWebHexWithAlpha(stop.getColor()));
@@ -228,13 +274,13 @@ public class JVGLayer extends SVGPath {
         g.put(JsonKeys.CENTER_X.key, gradient.getCenterX());
         g.put(JsonKeys.CENTER_Y.key, gradient.getCenterY());
         g.put(JsonKeys.PROPORTIONAL.key, gradient.isProportional());
-        g.put(JsonKeys.CYCLE_METHOD.key, switch (gradient.getCycleMethod()){
-          case NO_CYCLE ->  _NO_CYCLE;
-          case REFLECT ->  _REFLECT;
+        g.put(JsonKeys.CYCLE_METHOD.key, switch (gradient.getCycleMethod()) {
+          case NO_CYCLE -> _NO_CYCLE;
+          case REFLECT -> _REFLECT;
           case REPEAT -> _REPEAT;
         });
         var stops = mapper.createArrayNode();
-        for(var stop:gradient.getStops()){
+        for (var stop : gradient.getStops()) {
           var s = mapper.createObjectNode();
           s.put(JsonKeys.OFFSET.key, stop.getOffset());
           s.put(JsonKeys.COLOR.key, toWebHexWithAlpha(stop.getColor()));
@@ -250,7 +296,7 @@ public class JVGLayer extends SVGPath {
     objectNode.put(JsonKeys.CONTENT.key, getContent());
     if (getClip() != null)
       objectNode.put(JsonKeys.CLIP.key, true);
-    switch (getEffect()){
+    switch (getEffect()) {
       case GaussianBlur blur -> {
         var effect = mapper.createObjectNode();
         effect.put(JsonKeys.TYPE.key, JsonKeys.GAUSSIAN_BLUR.key);
@@ -261,7 +307,7 @@ public class JVGLayer extends SVGPath {
       case null, default -> {
       }
     }
-    if(getBlendMode()!=null)
+    if (getBlendMode() != null)
       objectNode.put(JsonKeys.BLEND_MODE.key, getBlendMode().toString());
 
     return objectNode;
@@ -284,29 +330,30 @@ public class JVGLayer extends SVGPath {
     setStrokeWidth(objectNode.get(JsonKeys.STROKE_WIDTH.key).asDouble());
     setStroke(Color.web(objectNode.get(JsonKeys.STROKE.key).asText()));
 
-    switch (objectNode.get(JsonKeys.FILL.key)){
+    switch (objectNode.get(JsonKeys.FILL.key)) {
       case TextNode text -> setFill(Color.web(text.asText()));
       case ObjectNode obj -> {
         var stops = new ArrayList<Stop>();
-        var ss = (ArrayNode)obj.get(JsonKeys.STOPS.key);
-        for(var s:ss){
-          var stop = new Stop(s.get(JsonKeys.OFFSET.key).asDouble(),Color.web(s.get(JsonKeys.COLOR.key).asText()));
+        var ss = (ArrayNode) obj.get(JsonKeys.STOPS.key);
+        for (var s : ss) {
+          var stop = new Stop(s.get(JsonKeys.OFFSET.key).asDouble(), Color.web(s.get(JsonKeys.COLOR.key).asText()));
           stops.add(stop);
         }
-        CycleMethod cycleMethod = switch (obj.get(JsonKeys.CYCLE_METHOD.key).asText()){
+        CycleMethod cycleMethod = switch (obj.get(JsonKeys.CYCLE_METHOD.key).asText()) {
           case _NO_CYCLE -> CycleMethod.NO_CYCLE;
           case _REFLECT -> CycleMethod.REFLECT;
           case _REPEAT -> CycleMethod.REPEAT;
-          default -> throw new RuntimeException("unsupported cycleMethod type: " + obj.get(JsonKeys.CYCLE_METHOD.key).asText());
+          default ->
+            throw new RuntimeException("unsupported cycleMethod type: " + obj.get(JsonKeys.CYCLE_METHOD.key).asText());
         };
         boolean proportional = obj.get(JsonKeys.PROPORTIONAL.key).asBoolean();
-        switch (obj.get(JsonKeys.GRADIENT_TYPE.key).asText()){
+        switch (obj.get(JsonKeys.GRADIENT_TYPE.key).asText()) {
           case _LINEAR_GRADIENT -> {
             double startX = obj.get(JsonKeys.START_X.key).asDouble();
             double startY = obj.get(JsonKeys.START_Y.key).asDouble();
             double endX = obj.get(JsonKeys.END_X.key).asDouble();
             double endY = obj.get(JsonKeys.END_Y.key).asDouble();
-            setFill(new LinearGradient(startX, startY,endX,endY,proportional,cycleMethod,stops));
+            setFill(new LinearGradient(startX, startY, endX, endY, proportional, cycleMethod, stops));
           }
           case _RADIACAL_GRADIENT -> {
             double centerX = obj.get(JsonKeys.CENTER_X.key).asDouble();
@@ -314,9 +361,10 @@ public class JVGLayer extends SVGPath {
             double radius = obj.get(JsonKeys.RADIUS.key).asDouble();
             double focusAngle = obj.get(JsonKeys.FOCUS_ANGLE.key).asDouble();
             double focusDistance = obj.get(JsonKeys.FOCUS_DISTANCE.key).asDouble();
-            setFill(new RadialGradient(focusAngle,focusDistance,centerX,centerY,radius,proportional,cycleMethod,stops));
+            setFill(new RadialGradient(focusAngle, focusDistance, centerX, centerY, radius, proportional, cycleMethod, stops));
           }
-          default -> throw new RuntimeException("unsupported gradient type: " + obj.get(JsonKeys.GRADIENT_TYPE.key).asText());
+          default ->
+            throw new RuntimeException("unsupported gradient type: " + obj.get(JsonKeys.GRADIENT_TYPE.key).asText());
         }
       }
       default -> throw new RuntimeException("unsupported fill type: " + objectNode.get(JsonKeys.FILL.key));
@@ -345,24 +393,25 @@ public class JVGLayer extends SVGPath {
     makeSVGPathElement(element, cachePoints);
     update(content.endsWith("Z") ? "Z" : "");
 
-    if(objectNode.has(JsonKeys.EFFECT.key)){
+    if (objectNode.has(JsonKeys.EFFECT.key)) {
       var obj = objectNode.get(JsonKeys.EFFECT.key);
-      if(obj instanceof ObjectNode effect){
-        switch (effect.get(JsonKeys.TYPE.key).asText()){
+      if (obj instanceof ObjectNode effect) {
+        switch (effect.get(JsonKeys.TYPE.key).asText()) {
           case _GAUSSIAN_BLUR -> {
             var blur = new GaussianBlur(effect.get(JsonKeys.RADIUS.key).asDouble());
             setEffect(blur);
           }
-          default -> {}
+          default -> {
+          }
         }
       }
     }
 
-    if(objectNode.has(JsonKeys.BLEND_MODE.key)){
+    if (objectNode.has(JsonKeys.BLEND_MODE.key)) {
       var blendModeString = objectNode.get(JsonKeys.BLEND_MODE.key).asText();
 
-      for(var blendMode:BlendMode.values()){
-        if(blendMode.toString().equals(blendModeString)){
+      for (var blendMode : BlendMode.values()) {
+        if (blendMode.toString().equals(blendModeString)) {
           setBlendMode(blendMode);
           break;
         }
